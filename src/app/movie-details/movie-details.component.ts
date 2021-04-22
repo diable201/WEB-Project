e9fc6745f32e108789616c7f727f3e7d46e47b99
Allmaz <almaizk0201@gmail.com>
2021-04-22 19:46:15 +0500

    Onclick events
    Added buttons

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {MovieService} from '../movie.service';
 import {movies} from '../movies';
+import {Location} from '@angular/common';
+
 // import {CartService} from '../cart.service';
 @Component({
   selector: 'app-movie-details',
@@ -10,7 +12,7 @@ import {movies} from '../movies';
 })
 export class MovieDetailsComponent implements OnInit {
   movie;
-
+  @Input() isLiked = false;
   // addToCart(product): void {
   //   this.cartService.addToCart(product);
   //   window.alert('Your product has been added to the cart!');
@@ -19,6 +21,7 @@ export class MovieDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private moviesService: MovieService,
+    private location: Location,
     // private cartService: CartService
   ) {
   }
@@ -33,6 +36,21 @@ export class MovieDetailsComponent implements OnInit {
     // Find the product that correspond with the id provided in route.
     this.movie = movies.find(movie => movie.id === movieIdFromRoute);
   }
-
-
+  goBack(): void {
+    this.location.back();
+  }
+  Onclick(movie: any): void{
+    if (this.isLiked === false){
+      this.movie.like += 1;
+      this.isLiked = true;
+    }else{
+      this.movie.like -= 1;
+      this.isLiked = false;
+    }
+  }
+  share(movie): void {
+    window.alert(`The movie ${movie.name} has been shared!`);
+    window.open(`https://t.me/share/url?url=http://localhost:4200/movies/${movie.id}&text=Hi! Watch this movie ${movie.name} on the Geek Street.`);
+  }
 }
+

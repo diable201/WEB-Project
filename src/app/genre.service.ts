911699ea26da4140d900b4f4e44c5211cbac1847
Sanzhar <diable201@protonmail.com>
2021-04-24 05:11:04 +0600

    Services

--- src/app/genre.service.ts
+++ src/app/genre.service.ts
@@ -2,20 +2,24 @@ import {Injectable} from '@angular/core';
 import {genres} from './genres';
 import {Observable, of} from 'rxjs';
 import {Genre} from './genres';
+import {HttpClient} from '@angular/common/http';
+import {Movie} from './movies';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenreService {
 
-  constructor() {
+  constructor(private client: HttpClient) {
   }
-
+  BASE_URL = 'http://localhost:4200';
   getGenre(id: any): Observable<Genre> {
+    // return this.client.get<Genre>(`${this.BASE_URL}/genre/genreId`);
     return of(genres.find(genre => genre.id === id));
   }
 
   getGenres(): Observable<Genre[]> {
+    // return this.client.get<Genre[]>(`${this.BASE_URL}/genre`);
     return of(genres);
   }
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -10,11 +10,14 @@ import {movies} from './movies';
 export class MovieService {
   constructor(private client: HttpClient) {
   }
+  BASE_URL = 'http://localhost:4200';
   getMovies(): Observable<Movie[]> {
-    return of(movies);
+    // return of(movies);
+    return this.client.get<Movie[]>(`${this.BASE_URL}/movies`);
   }
 
   getMoviesByGenreId(id: number): Observable<Movie[]> {
+    // return this.client.get<Movie[]>(`${this.BASE_URL}/genre/${id}`);
     return of(movies.filter(movie => movie.genreId === id));
   }
 }
